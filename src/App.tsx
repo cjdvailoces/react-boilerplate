6006b66a30fa4f50e0337ca87879928b67da4f3b
CJ Vailoces <christianv@affinityteam.com>
2023-06-21 17:30:48 +0800

    Add Ant.Design Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./styles/index.scss";
 import { ClickCounter } from "./components/ClickCounter";
+import { Button } from "antd";
 
 export const App = () => {
   const name = "CJDV";
   return (
     <div>
       <h1>Hi React Typescript! Hey yoww {name}!</h1>
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
       <ClickCounter />{" "}
     </div>
   );
